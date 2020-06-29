3f9c8c14a9815a25efc4f982f72c19859eba23ac
gmul <gmul.choi@gmail.com>
2020-06-29 23:59:30 +0900

    add all

---
+++ src/dynamic/ExpressInN.java
@@ -0,0 +1,17 @@
+package dynamic;
+
+
+public class ExpressInN {
+
+    public static void main(String[] args) {
+        int[] lost = {1,5};
+        int[] reserve = {3};
+
+        System.out.println(solution(1, 2));
+    }
+
+    public static int solution(int N, int number) {
+        return 0;
+    }
+}
+
